1a4f6b2aaaac59ea4117f6881520325b21e282a1
slemony <40449057+slemony@users.noreply.github.com>
2018-08-26 15:27:12 +0800

    Done

--- BeginScreen.js
+++ BeginScreen.js
@@ -14,29 +14,27 @@ import {
 } from 'react-native';
 
 export default class BeginScreen extends Component<Props> {
-
+  static navigationOptions = { header: null,};
   render() {
 
     return (
       <View style={styles.container}>
+
       <Text style={styles.header}>TIC TAC TOE</Text>
       <Text style={styles.sub}>The simplist game on the world</Text>
-      <View style={styles.button}>
+
+      <View style={styles.buttonsty}>
       <TouchableHighlight underlayColor="white"
       onPress={() => {this.props.navigation.navigate('ChoosePlayer')}}>
+      <View style={styles.button}>
         <Text style={styles.buttonText}>Start Game</Text>
+      </View>
       </TouchableHighlight>
       </View>
 
-
-
-
       </View>
-
     );
-
   }
-
 }
 
 const styles = StyleSheet.create({
@@ -44,25 +42,29 @@ const styles = StyleSheet.create({
     paddingTop: 0,
     alignItems: 'center'
   },
-  button: {
-    marginTop: 120,
-    marginBottom: 30,
-    width: 260,
-    alignItems: 'center',
-    backgroundColor: '#2196F3'
-  },
-  buttonText: {
-    padding: 20,
-    color: 'white'
-  },
   header: {
     color: 'black',
-    marginTop: 160,
+    marginTop: 190,
     padding:  0,
     fontWeight: 'bold',
     fontSize: 50
   },
   sub: {
     marginTop: 20
+  },
+  buttonsty: {
+    marginTop: 120,
+    marginBottom: 30,
+    width: 260,
+    alignItems: 'center',
+    backgroundColor: '#2196F3'
+  },
+  button:{
+    alignItems: 'center',
+    width: 260
+  },
+  buttonText: {
+    padding: 20,
+    color: 'white'
   }
 })
