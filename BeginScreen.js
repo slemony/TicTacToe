ff29b1ebfad9bf5b7d18cc091a0d2d79833a425a
slemony <40449057+slemony@users.noreply.github.com>
2018-08-26 12:54:00 +0800

    updated, styling still fixing

--- BeginScreen.js
+++ BeginScreen.js
@@ -1,16 +1,68 @@
 import React, { Component } from 'react';
 import {
-  Text,
-  View
+    Alert,
+    Platform,
+    StyleSheet,
+    Text,
+    TouchableHighlight,
+    TouchableOpacity,
+    TouchableNativeFeedback,
+    TouchableWithoutFeedback,
+    View,
+    FlatList,
+    AppState
 } from 'react-native';
 
-type Props = {};
 export default class BeginScreen extends Component<Props> {
+
   render() {
+
     return (
-      <View>
-        <Text>Begin Screen</Text>
+      <View style={styles.container}>
+      <Text style={styles.header}>TIC TAC TOE</Text>
+      <Text style={styles.sub}>The simplist game on the world</Text>
+      <View style={styles.button}>
+      <TouchableHighlight underlayColor="white"
+      onPress={() => {this.props.navigation.navigate('ChoosePlayer')}}>
+        <Text style={styles.buttonText}>Start Game</Text>
+      </TouchableHighlight>
       </View>
+
+
+
+
+      </View>
+
     );
+
   }
+
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 0,
+    alignItems: 'center'
+  },
+  button: {
+    marginTop: 120,
+    marginBottom: 30,
+    width: 260,
+    alignItems: 'center',
+    backgroundColor: '#2196F3'
+  },
+  buttonText: {
+    padding: 20,
+    color: 'white'
+  },
+  header: {
+    color: 'black',
+    marginTop: 160,
+    padding:  0,
+    fontWeight: 'bold',
+    fontSize: 50
+  },
+  sub: {
+    marginTop: 20
+  }
+})
